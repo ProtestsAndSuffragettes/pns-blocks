3224b7aa0665ffb280027eab7ecac9165497fd1d
Benjamin Rush <bnjmnrsh@gmail.com>
2026-08-10 15:11:38 +0100

    fix(blocks): align mobile content rails

--- build/blocks/layout/split-section/index.asset.php
+++ build/blocks/layout/split-section/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3b07916b6693ac67f55f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c84466d07973c648e80d');
