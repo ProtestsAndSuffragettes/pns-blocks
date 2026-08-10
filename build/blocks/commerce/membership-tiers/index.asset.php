721440440e36641818adfb3b3892d995f5193cc5
Benjamin Rush <bnjmnrsh@gmail.com>
2026-08-10 13:24:17 +0100

    fix: update membership editor styles ++

--- build/blocks/commerce/membership-tiers/index.asset.php
+++ build/blocks/commerce/membership-tiers/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'fb3d9e3a50548bf86b2c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '3f31185eb0a35b819f4d');
